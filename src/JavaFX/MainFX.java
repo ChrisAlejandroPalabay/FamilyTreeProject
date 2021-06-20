01395b6cb46b4515b0563cb99c8f5678bca28155
Chris Alejandro Palabay <2191948@slu.edu.ph>
2021-06-20 15:18:30 +0800

    Exploring how to generate Family members tree

--- src/JavaFX/MainFX.java
+++ src/JavaFX/MainFX.java
@@ -1,8 +1,15 @@
 package JavaFX;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -14,11 +21,22 @@ public class MainFX extends Application {
 
     public void start(Stage stage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+        stage.setTitle("Stack Pane");
+        stage.setWidth(600);
+        stage.setWidth(600);
+
+        ImageView img = new ImageView("JavaFX/trees.jpg");
+        img.setFitHeight(80);
+        img.setFitWidth(80);
+        Label label = new Label("Tress");
+        StackPane root = new StackPane(img,label);
+
+        root.setLayoutX(313);
+        root.setLayoutY(240);
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("Recap JavaFX");
         stage.show();
+
     }
 
 
