54445aa76feed6110c956cca33d874fcf3f9d272
Chris Alejandro Palabay <2191948@slu.edu.ph>
2021-06-22 11:52:26 +0800

    Generating FamilyMember tabs

--- src/JavaFX/MainFX.java
+++ src/JavaFX/MainFX.java
@@ -18,36 +18,15 @@ import javafx.stage.Stage;
 
 public class MainFX extends Application {
 
+    public static Parent root;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     public void start(Stage stage) throws Exception{
 
-        stage.setTitle("Stack Pane");
-        stage.setWidth(800);
-        stage.setHeight(800);
-
-        ImageView img = new ImageView("draku.jpg");
-        img.setFitHeight(80);
-        img.setFitWidth(80);
-        Label label = new Label("Draku");
-        Label label2 = new Label("Draku");
-        Label label3 = new Label("Draku");
-
-        VBox vbox = new VBox(label,label2,label3);
-
-        HBox hbox = new HBox(img, vbox);
-        hbox.prefHeight(60);
-        hbox.prefWidth(200);
-        hbox.setLayoutX(200);
-        hbox.setLayoutY(200);
-        hbox.setStyle("-fx-border-color: black;");
-
-        Group root = new Group();
-        root.getChildren().add(hbox);
-
-
+        root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
--- src/JavaFX/Scene1Controller.java
+++ src/JavaFX/Scene1Controller.java
@@ -3,27 +3,48 @@ package JavaFX;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.awt.*;
 
 
 public class Scene1Controller {
 
-    public HBox box;
+    //X=60
+    //Y=50
+
     public double x;
     public double y;
 
     @FXML
-    public void up(ActionEvent e){
-        box.setLayoutX(x+=4);
+    public void add(ActionEvent e){
+        ImageView img = new ImageView("draku.jpg");
+        img.setFitWidth(60);
+        img.setFitWidth(60);
+
+        Label lab1 = new Label("Draku");
+        Label lab2 = new Label("Oct 7 1999");
+        Label lab3 = new Label("The dog");
+
+        VBox vBox = new VBox(lab1,lab2,lab3);
+        HBox hbox = new HBox(img,vBox);
+        hbox.prefHeight(60);
+        hbox.prefWidth(200);
+        hbox.setStyle("-fx-border-color: black;");
+        hbox.setLayoutX(x+=30);
+        hbox.setLayoutY(y);
+
+        MainFX.root.getChildrenUnmodifiable().add(hbox);
+
     }
-    public void down(ActionEvent e){
-        box.setLayoutX(x-=4);
-    }
-    public void left(ActionEvent e){
-        box.setLayoutY(y-=4);
-    }
-    public void right(ActionEvent e){
-        box.setLayoutY(y+=4);
+    public void nextLine(ActionEvent e){
+        y =+ 40;
     }
 }
